33df86f0752029fda7d3165bfd0d86042eaefc4b
Rob <rbt.maldonado@gmail.com>
2021-05-24 11:14:59 -0400

    Completed calculator

--- pycalc.py
+++ pycalc.py
@@ -5,6 +5,7 @@
 """PyCalc is a simple calculator built using Python and PyQt5."""
 
 import sys
+from functools import partial
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QGridLayout
@@ -19,6 +20,7 @@ from PyQt5.QtWidgets import QWidget
 __version__ = '0.1'
 __author__ = 'Leodanis Pozo Ramos'
 
+ERROR_MSG = 'ERROR'
 # Create a subclass of QMainWindow to setup the calculator's GUI
 class PyCalcUi(QMainWindow):
     """PyCalc's View (GUI)."""
@@ -85,6 +87,62 @@ class PyCalcUi(QMainWindow):
         # Add buttonsLayout to the general layout
         self.generalLayout.addLayout(buttonsLayout)
 
+    def setDisplayText(self, text):
+        """Set display's text."""
+        self.display.setText(text)
+        self.display.setFocus()
+
+    def displayText(self):
+        """Get display's text."""
+        return self.display.text()
+
+    def clearDisplay(self):
+        """Clear the display."""
+        self.setDisplayText('')
+
+# Create a Controller class to connect the GUI and the model
+class PyCalcCtrl:
+    """PyCalc Controller class."""
+    def __init__(self, model, view):
+        """Controller initializer."""
+        self._evaluate = model
+        self._view = view
+        # Connect signals and slots
+        self._connectSignals()
+
+    def _calculateResult(self):
+        """Evaluate expressions."""
+        result = self._evaluate(expression=self._view.displayText())
+        self._view.setDisplayText(result)
+
+    def _buildExpression(self, sub_exp):
+        """Build expression."""
+        if self._view.displayText() == ERROR_MSG:
+          self._view.clearDisplay()
+
+        expression = self._view.displayText() + sub_exp
+        self._view.setDisplayText(expression)
+
+    def _connectSignals(self):
+        """Connect signals and slots."""
+        for btnText, btn in self._view.buttons.items():
+            if btnText not in {'=', 'C'}:
+                btn.clicked.connect(partial(self._buildExpression, btnText))
+                
+        self._view.buttons['='].clicked.connect(self._calculateResult)
+        self._view.display.returnPressed.connect(self._calculateResult)
+        self._view.buttons['C'].clicked.connect(self._view.clearDisplay)
+        
+# Create a Model to handle the calculator's operation
+def evaluateExpression(expression):
+    """Evaluate an expression."""
+    try:
+        result = str(eval(expression, {}, {}))
+    except Exception:
+        result = ERROR_MSG
+
+    return result
+
 # Client code
 def main():
     """Main function."""
@@ -93,6 +151,10 @@ def main():
     # Show the calculator's GUI
     view = PyCalcUi()
     view.show()
+     # Create instances of the model and the controller
+    # PyCalcCtrl(view=view)
+    model = evaluateExpression
+    PyCalcCtrl(model=model, view=view)
     # Execute the calculator's main loop
     sys.exit(pycalc.exec_())
 
